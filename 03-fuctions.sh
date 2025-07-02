9844f5962a5ef8dba906d561b5392dd11939a4c7
Geethikaakula <akulageethika6@gmail.com>
2025-07-02 11:16:20 +0530

    sample code

---
+++ 03-fuctions.sh
@@ -0,0 +1,17 @@
+# if we assign a name to set of commands then it is a function.
+
+#Syntax---
+# function function_name() {
+
+ #commands
+
+ }
+
+ #example
+ sample() {
+   echo sample function
+ }
+
+ #and how to call this functions is by only by the name of the function
+
+ sample
